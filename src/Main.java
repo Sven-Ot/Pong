c3ce273d60ee610ea04d6b15f9766ec10fa159dc
arthurXpro <65346354+arthurXpro@users.noreply.github.com>
2021-03-04 15:14:45 +0100

    Anpassungen

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+package src;
+
+import src.Fenster;
+
 public class Main {
-    public static void main(String args[]){
+    public static void main(String[] args){
         Fenster fenster = new Fenster();
         Thread t = new Thread();
         t.start();
