2d7d63c765ae625ce5a76b6e4ee91e81e98e8e9b
Suption19 <62257564+Suption19@users.noreply.github.com>
2021-03-13 23:02:28 +0100

    Added KeyListener to move player panel

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import src.GameWindow;
 
-public class Main {
+public class Main{
     public static void main(String[] args){
         GameWindow window = new GameWindow(null,null);
     }  
--- src/src/GameWindow.java
+++ src/src/GameWindow.java
@@ -2,31 +2,59 @@ package src;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import jdk.internal.net.http.common.Pair;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class GameWindow{
+public class GameWindow implements KeyListener{
     public JFrame window; 
+    public JPanel leftPanel; 
+    public JPanel rightPanel;
     public GameWindow(Player player, Bot bot){
+        ImageIcon img = new ImageIcon("images/PongImg.png");
         double[] resolution = GetResolution();
         window = new JFrame("Pong");    
-        JPanel rightPanel =new JPanel();  
+        rightPanel = new JPanel();  
+        leftPanel = new JPanel();  
         rightPanel.setBounds(200,10,15,200);    
         rightPanel.setBackground(Color.black);  
+        leftPanel = GetLeftPanel(resolution);
         window.add(GetLeftPanel(resolution));  
-        window.add(GetLeftPanel(resolution));  
-        window.add(GetRightPanel(resolution));
+        rightPanel = GetRightPanel(resolution);
+        window.add(rightPanel);
         window.setSize((int)resolution[0],(int)resolution[1]);    
         window.setLayout(null);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setVisible(true);
         window.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+        window.getContentPane().setBackground(Color.black);
+        window.addKeyListener(this);
+        window.setIconImage(img.getImage());
+    }
+    
+    @Override
+    public void keyPressed(KeyEvent e) {
+        //TODO: Disable moving out of field
+        Rectangle rec = leftPanel.getBounds();
+        int key = e.getKeyCode();
+        if(key == KeyEvent.VK_W)
+            leftPanel.setLocation(rec.x,rec.y-5);
+        if(key == KeyEvent.VK_S)
+          leftPanel.setLocation(rec.x,rec.y+5);
+    }
+    
+    @Override
+    public void keyTyped(KeyEvent e) {
+        return;
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        return;
     }
     
     private double[] GetResolution(){
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        return new double[] {screenSize.getWidth(), screenSize.getHeight()};
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    return new double[] {screenSize.getWidth(), screenSize.getHeight()};
     }
     
     private JPanel GetLeftPanel(double[] resolution){
@@ -34,9 +62,8 @@ public class GameWindow{
         double width = resolution[0] - resolution[0] * 0.99;
         double panelHeight = resolution[1] * 0.3;
         double mid = resolution[1] / 2 - panelHeight / 2;
-        JPanel leftPanel =new JPanel();  
         leftPanel.setBounds((int)topLeft,(int)mid,(int)width, (int )panelHeight);    
-        leftPanel.setBackground(Color.black);  
+        leftPanel.setBackground(Color.white);  
         return leftPanel;
     }
     
@@ -45,9 +72,9 @@ public class GameWindow{
         double topRight = resolution[0] - resolution[0] / 100 *2 - width;
         double panelHeight = resolution[1] * 0.3;
         double mid = resolution[1] / 2 - panelHeight / 2;
-        JPanel leftPanel =new JPanel();  
-        leftPanel.setBounds((int)topRight,(int)mid,(int)width, (int )panelHeight);    
-        leftPanel.setBackground(Color.black);  
-        return leftPanel;
+        rightPanel.setBounds((int)topRight,(int)mid,(int)width, (int )panelHeight);    
+        rightPanel.setBackground(Color.white);  
+        return rightPanel;
     }
+
 }
